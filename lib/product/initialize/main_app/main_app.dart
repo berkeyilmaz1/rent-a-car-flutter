de3a947980a98e6fb1880573779c0b6efad34b54
berke.yz <berkeyilmaz189@gmail.com>
2024-11-26 18:28:22 +0300

    project initialized

---
+++ lib/product/initialize/main_app/main_app.dart
@@ -0,0 +1,17 @@
+// final class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp.router(
+//       localizationsDelegates: context.localizationDelegates,
+//       supportedLocales: context.supportedLocales,
+//       locale: context.locale,
+//       debugShowCheckedModeBanner: false,
+//       title: ProductConstants.appName,
+//       builder: _MainAppResponsive.builder,
+//       theme: ThemeManager.createTheme(AppThemeLight()),
+//       routerConfig: MainAppRouter.router,
+//     );
+//   }
+// }
---
+++ lib/product/utils/product_radius.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+final class ProductRadius {
+  const ProductRadius._();
+
+  /// value is 30
+  static const double profileRadius = 30;
+
+  /// value is 45
+  static const double profileRadiusHigh = 45;
+
+  /// value is 32
+  static const double normal = 32;
+
+  /// value is 27
+  static const double circleNormal = 27;
+
+  /// value is 20
+  static const double circleNormalLow = 20;
+}
