45d9c92d7a491c45052cf533946d2322bd5c5565
berke.yz <berkeyilmaz189@gmail.com>
2024-12-02 00:13:58 +0300

    selection view

---
+++ lib/features/selection/view/selection_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:rent_a_car/features/selection/view/mixin/selection_view_mixin.dart';
+import 'package:rent_a_car/product/widgets/page/page_padding.dart';
+import 'package:rent_a_car/product/widgets/widget_sizes.dart';
+
+part '../widgets/selection_card.dart';
+
+final class SelectionView extends StatefulWidget {
+  const SelectionView({super.key});
+
+  @override
+  State<SelectionView> createState() => _SelectionViewState();
+}
+
+class _SelectionViewState extends State<SelectionView> with SelectionViewMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2,
+              child: Image.asset(
+                "assets/images/car3.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const PagePadding.horizontalHighSymmetric(),
+            child: Center(
+              child: SelectionCard(
+                onPressed: selectDate,
+                formattedStartDate: formattedStartDate,
+                formattedEndDate: formattedEndDate,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
