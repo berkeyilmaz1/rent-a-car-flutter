14adac2265dca170665ddf0ced0129b7e74cc288
berke.yz <berkeyilmaz189@gmail.com>
2024-12-03 22:06:04 +0300

    car service

---
+++ lib/core/product_network_manager.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:rent_a_car/core/model/base_error_model.dart';
+import 'package:vexana/vexana.dart';
+
+final class ProductNetworkManager extends NetworkManager<BaseErrorModel> {
+  ProductNetworkManager()
+      : super(
+          isEnableLogger: true,
+          options: BaseOptions(
+            baseUrl: _baseUrl,
+            contentType: _contentType,
+            validateStatus: (status) {
+              return (status ?? 0) < HttpStatus.permanentRedirect;
+            },
+          ),
+        );
+  static const String _baseUrl = ' ';
+  static const String _contentType = 'application/json';
+}
