de301198e6741df74535daa4c4b723e93e744ea6
aleynayilm <alynylmazz@gmail.com>
2024-12-10 14:18:06 +0300

    scroll view added sign up

--- lib/features/auth/view/sign_up_view.dart
+++ lib/features/auth/view/sign_up_view.dart
@@ -27,127 +27,129 @@ class _SignUpViewState extends State<SignUpView> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const PagePadding.all(),
-      child: Column(
-        mainAxisSize: MainAxisSize.min, // İçeriğe göre boyutlandırma
-        children: [
-          // İsim ve Soyisim
-          Row(
-            children: [
-              Expanded(
-                child: TextFormField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Adınız',
-                    hintText: 'Adınızı giriniz',
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const PagePadding.all(),
+        child: Column(
+          mainAxisSize: MainAxisSize.min, // İçeriğe göre boyutlandırma
+          children: [
+            // İsim ve Soyisim
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Adınız',
+                      hintText: 'Adınızı giriniz',
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: WidgetSizes.spacingM),
-              Expanded(
-                child: TextFormField(
-                  controller: lastnameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Soyadınız',
-                    hintText: 'Soyadınızı giriniz',
+                const SizedBox(width: WidgetSizes.spacingM),
+                Expanded(
+                  child: TextFormField(
+                    controller: lastnameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Soyadınız',
+                      hintText: 'Soyadınızı giriniz',
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: WidgetSizes.spacingM),
-
-          // E-posta
-          TextFormField(
-            controller: emailController,
-            decoration: const InputDecoration(
-              labelText: 'E-Posta',
-              hintText: 'E-posta adresinizi giriniz',
+              ],
             ),
-          ),
-          const SizedBox(height: WidgetSizes.spacingM),
+            const SizedBox(height: WidgetSizes.spacingM),
 
-          // Şifre
-          TextFormField(
-            controller: passwordController,
-            decoration: InputDecoration(
-              labelText: 'Şifre',
-              hintText: 'Şifrenizi giriniz',
-              suffixIcon: IconButton(
-                icon: Icon(
-                  obscureText ? Icons.visibility : Icons.visibility_off,
+            // E-posta
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: 'E-Posta',
+                hintText: 'E-posta adresinizi giriniz',
+              ),
+            ),
+            const SizedBox(height: WidgetSizes.spacingM),
+
+            // Şifre
+            TextFormField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                labelText: 'Şifre',
+                hintText: 'Şifrenizi giriniz',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    obscureText ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      obscureText = !obscureText;
+                    });
+                  },
                 ),
-                onPressed: () {
-                  setState(() {
-                    obscureText = !obscureText;
-                  });
-                },
               ),
+              obscureText: obscureText,
             ),
-            obscureText: obscureText,
-          ),
-          const SizedBox(height: WidgetSizes.spacingM),
+            const SizedBox(height: WidgetSizes.spacingM),
 
-          // Şifre Tekrar
-          TextFormField(
-            controller: confirmPasswordController,
-            decoration: InputDecoration(
-              labelText: 'Şifre Tekrar',
-              hintText: 'Şifrenizi tekrar giriniz',
-              suffixIcon: IconButton(
-                icon: Icon(
-                  obscureText ? Icons.visibility : Icons.visibility_off,
+            // Şifre Tekrar
+            TextFormField(
+              controller: confirmPasswordController,
+              decoration: InputDecoration(
+                labelText: 'Şifre Tekrar',
+                hintText: 'Şifrenizi tekrar giriniz',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    obscureText ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      obscureText = !obscureText;
+                    });
+                  },
                 ),
-                onPressed: () {
-                  setState(() {
-                    obscureText = !obscureText;
-                  });
-                },
+              ),
+              obscureText: obscureText,
+            ),
+            const SizedBox(height: WidgetSizes.spacingM),
+
+            // Telefon Numarası
+            TextFormField(
+              controller: phoneNumberController,
+              decoration: const InputDecoration(
+                labelText: 'Telefon Numarası',
+                hintText: 'Telefon numaranızı giriniz',
               ),
             ),
-            obscureText: obscureText,
-          ),
-          const SizedBox(height: WidgetSizes.spacingM),
+            const SizedBox(height: WidgetSizes.spacingM),
 
-          // Telefon Numarası
-          TextFormField(
-            controller: phoneNumberController,
-            decoration: const InputDecoration(
-              labelText: 'Telefon Numarası',
-              hintText: 'Telefon numaranızı giriniz',
+            // Lisans Numarası
+            TextFormField(
+              controller: licenseNumberController,
+              decoration: const InputDecoration(
+                labelText: 'Sürücü Sicil Numarası',
+                hintText: 'Sürücü Sicil Numaranızı giriniz',
+              ),
             ),
-          ),
-          const SizedBox(height: WidgetSizes.spacingM),
+            const SizedBox(height: WidgetSizes.spacingM),
 
-          // Lisans Numarası
-          TextFormField(
-            controller: licenseNumberController,
-            decoration: const InputDecoration(
-              labelText: 'Sürücü Sicil Numarası',
-              hintText: 'Sürücü Sicil Numaranızı giriniz',
+            // Adres
+            TextFormField(
+              controller: addressController,
+              decoration: const InputDecoration(
+                labelText: 'Adres',
+                hintText: 'Adresinizi giriniz',
+              ),
             ),
-          ),
-          const SizedBox(height: WidgetSizes.spacingM),
+            const SizedBox(height: WidgetSizes.spacingXxl2),
 
-          // Adres
-          TextFormField(
-            controller: addressController,
-            decoration: const InputDecoration(
-              labelText: 'Adres',
-              hintText: 'Adresinizi giriniz',
+            // Kayıt Ol butonu
+            AuthButton(
+              onPressed: () {
+                const SelectionViewRoute().go(context);
+              },
+              buttonName: 'Kayıt Ol',
             ),
-          ),
-          const SizedBox(height: WidgetSizes.spacingXxl2),
-
-          // Kayıt Ol butonu
-          AuthButton(
-            onPressed: () {
-              const SelectionViewRoute().go(context);
-            },
-            buttonName: 'Kayıt Ol',
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
