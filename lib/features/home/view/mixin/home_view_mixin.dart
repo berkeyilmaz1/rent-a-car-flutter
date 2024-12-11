0bd80b8ad64fa96af17096c532c99121a2391aae
aleynayilm <alynylmazz@gmail.com>
2024-12-12 01:27:46 +0300

    dealer selection

--- lib/features/home/view/mixin/home_view_mixin.dart
+++ lib/features/home/view/mixin/home_view_mixin.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:rent_a_car/core/product_network_manager.dart';
 import 'package:rent_a_car/features/home/view/home_view.dart';
 import 'package:rent_a_car/product/initialize/service/models/car/car.dart';
+import 'package:rent_a_car/product/initialize/service/models/dealership/dealership.dart';
 import 'package:rent_a_car/product/initialize/service/rent_a_car_service.dart';
+import 'package:rent_a_car/product/utils/formatters/formatters.dart';
 
 mixin HomeViewMixin on State<HomeView> {
   late final TextEditingController minPriceController;
   late final TextEditingController maxPriceController;
 
+  String selectedSort = 'Varsayılan';
   String selectedFuelType = 'Hepsi';
   late final RentACarService _rentACarService;
   List<Car>? cars;
@@ -17,12 +20,14 @@ mixin HomeViewMixin on State<HomeView> {
   late final String endDate;
   late final DateTime startDateWithTime;
   late final DateTime endDateWithTime;
+  late final int selectedDealerId;
   @override
   void initState() {
     super.initState();
     dayCount = widget.parameters['dayCount'] as int;
     startDate = widget.parameters['startDate'] as String;
     endDate = widget.parameters['endDate'] as String;
+    selectedDealerId = widget.parameters['selectedDealership'] as int;
     startDateWithTime = widget.parameters['startDateWithTime'] as DateTime;
     endDateWithTime = widget.parameters['endDateWithTime'] as DateTime;
 
